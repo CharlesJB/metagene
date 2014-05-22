CharlesJB/metagene

commit f2957536639fef00ea3648a1d79b649641420215

R/calculate_intensity.R: Changed the parseBam function to make it more generic

R/calculate_intensity.R
@@ -296,7 +296,6 @@ parseBamFiles <- function(bamFiles, featuresGroups, groups, design=NULL, cores=1
 	parseGroup <- function(groupName, featuresGroups) {
 		print(paste("parseGroup:", groupName))
 		# Get bam files
-		#currentBamFiles <- unname(unlist(groups[[groupName]]$bamFiles))
 		currentBamFiles <- groups[[groupName]]$bamFiles
 
 		# Get features
@@ -304,7 +303,7 @@ parseBamFiles <- function(bamFiles, featuresGroups, groups, design=NULL, cores=1
 		currentFeatures <- featuresGroups[[currentFeatureName]]
 
 		# Parse each bam
-		currentBamFiles <- lapply(currentBamFiles, function(x) parseBamFile(x, bamFiles, currentFeatures, cores))
+		currentBamFiles <- lapply(currentBamFiles, function(x) parseBamFile(x, bamFiles[bamFiles$bam == x,]$alignedCount, currentFeatures, cores))
 		# Add the names
 		currentGroup <- list()
 		currentGroup$featureName <- groups[[groupName]]$featureName
@@ -325,7 +324,7 @@ parseBamFiles <- function(bamFiles, featuresGroups, groups, design=NULL, cores=1
 	# p < lapply(1:length(y), function(x) unlist(y[[x]]) - unlist(u[[x]]))
 }
 
-parseBamFile <- function(bamFile, bamFiles, features, cores=1) {
+parseBamFile <- function(bamFile, alignedCount, features, cores=1) {
 	print(paste("Current bam:", bamFile))
 	extractReadsDensity <- function(feature, bamFile) {
 		# Extract raw counts

commit 923fa94d2f7f0ab41da4a92b4eb9d295fc943763

R/calculate_intensity.R: corrected a bug introduced in f2957536639fef00ea3648a1d79b649641420215
Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

R/calculate_intensity.R
@@ -369,8 +369,7 @@ parseBamFile <- function(bamFile, alignedCount, features, cores=1) {
 		}
 
 		# Convert to RPM
-		currentAlignedCount <- bamFiles[bamFiles$bam == bamFile,]$alignedCount
-		vectorResult <- vectorResult / (currentAlignedCount / 1000000)
+		vectorResult <- vectorResult / (alignedCount / 1000000)
 		return(vectorResult)
 
 	}