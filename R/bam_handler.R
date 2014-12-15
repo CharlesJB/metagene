cca0de68f4999c77124e585ce9a64f92c99cc54a
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2014-12-15 15:39:16 -0500

    First complete version of Bam_Handler R6 class
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/bam_handler.R
+++ R/bam_handler.R
@@ -1,11 +1,13 @@
 # Class used to manage bam files
-bam_handler <- R6Class("bam_handler",
+Bam_Handler <- R6Class("Bam_Handler",
   public = list(
-    initialize = function(bam_files) {
+    bam_files = data.frame(),
+    bpparam = SerialParam(),
+    initialize = function(bam_files, cores = SerialParam()) {
       # Check prerequisites 
       # All BAM file names must be of string type
       if (sum(sapply(bam_files, is.character)) != length(bam_files)) {
-        stop("At least one BAM file name is not a valid name (a character string).")
+        stop("At least one BAM file name is not a character string.")
       }
 
       # All BAM files must exist
@@ -13,29 +15,54 @@ bam_handler <- R6Class("bam_handler",
         stop("At least one BAM file does not exist.")
       }
       
-      private$bam_files <<- data.frame(bam <- bam_files, old <- bam_files, aligned_count <- 0)
-      private$bam_files[["bam"]] <- sapply(bam_files, private$index_bam_file)
-      private$bam_files[["aligned_count"]] <- sapply(private$bam_files[["bam"]], private$get_file_count)
+      if (is.numeric(cores)) {
+        # The number of cores has to be a positive integer
+        if(as.integer(cores) != cores || cores <= 0) {
+          stop("The number of cores has to be a positive integer.")
+        }
+        if (cores == 1) {
+          BPPARAM <- SerialParam()
+        } else {
+          BPPARAM <- MulticoreParam(workers = cores)
+        }
+      } else {
+        # Must be one of the BiocParallelParam class
+        if (class(cores) != "SerialParam"
+            & class(cores) != "MulticoreParam"
+            & class(cores) != "SnowParam"
+            & class(cores) != "BatchJobParam"
+            & class(cores) != "DoparParam") {
+          stop("Param cores must be numeric or BiocParallelParam instance.")
+        }
+        BPPARAM <- cores
+      }
+      self$bpparam <- BPPARAM
+      self$bam_files <- data.frame(matrix(nrow = length(bam_files)))
+      colnames(self$bam_files) <- "old"
+      self$bam_files[["old"]] <- bam_files
+      self$bam_files[["bam"]] <- sapply(bam_files, private$index_bam_file)
+      self$bam_files[["aligned_count"]] <-
+        sapply(self$bam_files[["bam"]], private$get_file_count)
     },
-    get_aligned_counts = function(bam_file) {
+    get_aligned_count = function(bam_file) {
       # Check prerequisites
       # The bam file must be in the list of bam files used for initialization
-      if (! bam_file %in% private$bam_files[["old"]]) {
+      if (! bam_file %in% self$bam_files[["old"]]) {
         stop(paste0("Bam file ", bam_file, " not found."))
       }
-      i <- private$bam_files[["old"]] == bam_file
-      private$bam_files[["aligned_count"]][i]
+      i <- self$bam_files[["old"]] == bam_file
+      self$bam_files[["aligned_count"]][i]
     },
     get_rpm_coefficient = function(bam_file) {
-      self$get_aligned_counts(bam_file) / 1000000
+      return(self$get_aligned_count(bam_file) / 1000000)
     }
   ),
   private = list(
     index_bam_file = function(bam_file) {
       if (file.exists(paste(bam_file, ".bai", sep=""))  == FALSE) {
         # If there is no index file, we sort and index the current bam file
-        # TODO: we need to check if the sorted file was previously produced before
-        #       doing the costly sort operation
+        # TODO: we need to check if the sorted file was previously produced
+        #       before doing the costly sort operation
         sorted_bam_file <- paste0(basename(bam_files), ".sorted")
         sortBam(bam_file, sorted_bam_file)
         sorted_bam_file <- paste0(sorted_bam_file, ".bam")
---
+++ inst/unitTests/test_bam_handler.R
@@ -0,0 +1,164 @@
+## Test functions present in the bam_handler.R file
+
+### {{{ --- Test setup ---
+
+if(FALSE) {
+  library( "RUnit" )
+  library( "metagene" )
+}
+
+### }}}
+
+bam_files <- get_demo_bam_files()
+
+###################################################
+## Test the Bam_Handler$new() function (initialize)
+###################################################
+
+## Single valid bam file
+test.bam_handler_single_valid_file <- function() {
+  bam_handler <- Bam_Handler$new(bam_files[1])
+  checkTrue(all(class(bam_handler) == c("Bam_Handler", "R6")),
+            msg = "Bam_Handler initialize - Valid initialize call with one bam file did not return correct class")
+}
+
+## Multiple valid bam files, no cores
+test.bam_handler_valid_files_no_cores <- function() {
+  bam_handler <- Bam_Handler$new(bam_files = bam_files)
+  checkTrue(all(class(bam_handler) == c("Bam_Handler", "R6")),
+    msg = "Bam_Handler initialize - Valid initialize call with multiple bam files did not return correct class")
+}
+
+## Valid bam files, numeric cores
+test.bam_handler_valid_files_numeric_cores <- function() {
+  bam_handler <- Bam_Handler$new(bam_files = bam_files, cores = 2)
+  checkTrue(all(class(bam_handler) == c("Bam_Handler", "R6")),
+            msg = "Bam_Handler initialize - Valid initialize call with multiple bam files and numeric cores did not return correct class")
+}
+
+## Valid bam files, bpparam cores
+test.bam_handler_valid_files_numeric_cores <- function() {
+  bam_handler <- Bam_Handler$new(bam_files = bam_files, cores = MulticoreParam(workers = 2))
+  checkTrue(all(class(bam_handler) == c("Bam_Handler", "R6")),
+            msg = "Bam_Handler initialize - Valid initialize call with multiple bam files and bpparam cores did not return correct class")
+}
+
+## Zero core should not be accepted as an argument
+test.bam_handler_initialize_zero_core_number<- function() {
+  obs <- tryCatch(Bam_Handler$new(bam_files = bam_files, cores = 0), error=conditionMessage)
+  exp <- "The number of cores has to be a positive integer."
+  obs <- tryCatch(Bam_Handler$new(bam_files = bam_files, cores = -1), error=conditionMessage)
+  exp <- "The number of cores has to be a positive integer."
+  checkIdentical(obs, exp,
+    msg = "Bam_Handler initialize - A negative core number argument did not generate an exception with expected message.")
+}
+
+## Something other than an integer number should not be accepted as an core number argument
+test.bam_handler_initialize_not_integer_core_number<- function() {
+  obs <- tryCatch(Bam_Handler$new(bam_files = bam_files,  cores = 2.22), error=conditionMessage)
+  exp <- "The number of cores has to be a positive integer."
+  checkIdentical(obs, exp,
+    msg = "Bam_Handler initialize - A decimal core number argument did not generate an exception with expected message.")
+}
+
+## Something other than an integer number should not be accepted as an core number argument
+test.bam_handler_initialize_string_core_number<- function() {
+  obs <- tryCatch(Bam_Handler$new(bam_files = bam_files,  cores ="NotAInteger"), error=conditionMessage)
+  exp <- "Param cores must be numeric or BiocParallelParam instance."
+  checkIdentical(obs, exp,
+    msg = "Bam_Handler initialize - A generic text used as a core number did not generate an exception with expected message.")
+}
+
+## All bam files must be in string format
+test.bam_handler_initialize_file_name_not_in_string_format<- function() {
+  obs <- tryCatch(Bam_Handler$new(bam_files = c(1,2)), error = conditionMessage)
+  exp <- "At least one BAM file name is not a character string."
+  checkEquals(obs, exp,
+    msg = "Bam_Handler initialize - Integers used as files argument did not generate an exception with expected message.")
+}
+
+## All bam files must exist
+test.bam_handler_initialize_not_existing_files<- function() {
+  obs <- tryCatch(Bam_Handler$new(bam_files = c("NotExistingFile", "NotExistingFile2")), error = conditionMessage)
+  exp <- "At least one BAM file does not exist."
+  checkEquals(obs, exp,
+    msg = "Bam_Handler initialize - Not existing BAM file used as argument did not generate an exception with expected message.")
+}
+
+###################################################
+## Test the bam_handler$get_aligned_count()
+###################################################
+
+# Note, count were obtained with "samtools view -c -F0x4 ${file}"
+exp <- list(4635, 1896, 956, 1999)
+names(exp) <- bam_files
+
+## Valid use case
+test.bam_handler_get_aligned_count_valid_case <- function() {
+  # Note, count were obtained with "samtools view -c -F0x4 ${file}"
+  exp <- list(4635, 1896, 956, 1999)
+  names(exp) <- bam_files
+  bam_handler <- Bam_Handler$new(bam_files = bam_files)
+  obs <- lapply(bam_files, bam_handler$get_aligned_count)
+  names(obs) <- bam_files
+  checkTrue(all(mapply("==", obs, exp)),     
+    msg = "Bam_Handler get_aligned_count valid case does not give the expected aligned count")
+}
+
+## Valid use case, multicore
+test.bam_handler_get_aligned_count_valid_case_multicore <- function() {
+  # Note, count were obtained with "samtools view -c -F0x4 ${file}"
+  exp <- list(4635, 1896, 956, 1999)
+  names(exp) <- bam_files
+  bam_handler <- Bam_Handler$new(bam_files = bam_files, cores = 2)
+  obs <- lapply(bam_files, bam_handler$get_aligned_count)
+  names(obs) <- bam_files
+  checkTrue(all(mapply("==", obs, exp)),
+    msg = "Bam_Handler get_aligned_count valid case does not give the expected aligned count")
+}
+
+## Invalid bam file
+test.bam_handler_get_aligned_count_invalid_bam_file <- function() {
+  bam_handler <- Bam_Handler$new(bam_files = bam_files)
+  obs <- tryCatch(bam_handler$get_aligned_count(bam_file = "not_a_valid_bam_file"), error=conditionMessage)
+  exp <- "Bam file not_a_valid_bam_file not found."
+  checkEquals(obs, exp,
+    msg = "Bam_Handler get_aligned_count invalid bam file does not give the expected error message")
+}
+
+###################################################
+## Test the bam_handler$get_rpm_coefficient()
+###################################################
+
+## Valid use case
+test.bam_handler_get_rpm_coefficient_valid_case <- function() {
+  # Note, count were obtained with "samtools view -c -F0x4 ${file}"
+  exp <- list(4635/1000000, 1896/1000000, 956/1000000, 1999/1000000)
+  names(exp) <- bam_files
+  bam_handler <- Bam_Handler$new(bam_files = bam_files)
+  obs <- lapply(bam_files, bam_handler$get_rpm_coefficient)
+  names(obs) <- bam_files
+  checkTrue(all(mapply("==", obs, exp)),
+            msg = "Bam_Handler get_aligned_count valid case does not give the expected aligned count")
+}
+
+## Valid use case, multicore
+test.bam_handler_get_rpm_coefficient_valid_case_multicore <- function() {
+  # Note, count were obtained with "samtools view -c -F0x4 ${file}"
+  exp <- list(4635/1000000, 1896/1000000, 956/1000000, 1999/1000000)
+  names(exp) <- bam_files
+  bam_handler <- Bam_Handler$new(bam_files = bam_files, cores = 2)
+  obs <- lapply(bam_files, bam_handler$get_rpm_coefficient)
+  names(obs) <- bam_files
+  checkTrue(all(mapply("==", obs, exp)),
+            msg = "Bam_Handler get_aligned_count valid case does not give the expected aligned count")
+}
+
+## Invalid bam file
+test.bam_handler_get_rpm_coefficient_invalid_bam_file <- function() {
+  bam_handler <- Bam_Handler$new(bam_files = bam_files)
+  obs <- tryCatch(bam_handler$get_rpm_coefficient(bam_file = "not_a_valid_bam_file"), error=conditionMessage)
+  exp <- "Bam file not_a_valid_bam_file not found."
+  checkEquals(obs, exp,
+              msg = "Bam_Handler get_aligned_count invalid bam file does not give the expected error message")
+}
