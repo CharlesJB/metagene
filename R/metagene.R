586de8712c55bbb8692c1fa05736554447bc2f01
c.beauparlant <c.beauparlant@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-02-05 16:29:26 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: 3b3e0132bc80c57a0d55cc48e7bc7578d3c2f1c2
    
        R/metagene.R: Regions can no longer have negative start after padding
    
        Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
        
    Committed by: Charles Joly Beauparlant
    Author Name: Charles Joly Beauparlant
    Commit date: 2015-02-05 11:29:18 -0500
    Author date: 2015-02-05 11:29:18 -0500
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagene@99080 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/metagene.R
+++ R/metagene.R
@@ -234,6 +234,7 @@ metagene <- R6Class("metagene",
       GRangesList(lapply(regions, function(x) {
         # Add padding
         start(x) <- start(x) - self$params$padding_size
+	start(x)[start(x) < 0] <- 0
         end(x) <- end(x) + self$params$padding_size
         # Clean seqlevels
         seqlevels(x) <- unique(as.character(seqnames(x)))
