a7516d63ed69a5ae986552b68a4e3bf432687b25
Charles Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2015-02-13 14:22:18 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commits.
    Commit information:
    
    Commit id: 974adb0e6393f8996557e6efcd69d76b1dc3da7b
    
        Merge pull request #31 from adeschen/master
    
        Unit tests for metagene class.
    Committed by: CharlesJB
    Author Name: CharlesJB
    Commit date: 2015-02-13 09:21:59 -0500
    Author date: 2015-02-13 09:21:59 -0500
    
    Commit id: 159bdad0f89801c0a71d95329e2879b2bff28d69
    
        Shortening lines with more than 80 characters long. Adding gplots as import in NAMESPACE. Adding unit tests for metagene class.
    
    Committed by: adeschen
    Author Name: adeschen
    Commit date: 2015-02-12 17:34:57 -0500
    Author date: 2015-02-12 17:34:57 -0500
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagene@99455 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_metagene.R
@@ -0,0 +1,48 @@
+## Test functions present in the metagene.R file
+
+### {{{ --- Test setup ---
+
+if(FALSE) {
+    library( "RUnit" )
+    library( "metagene" )
+}
+
+### }}}
+
+
+###################################################
+## Test the metagene$new() function (initialize)
+###################################################
+
+base_msg <- "metagene initialize - "
+
+## Invalid verbose value
+test.metagene_initialize_invalid_verbose_value <- function() {
+    obs <- tryCatch(metagene:::metagene$new(verbose="ZOMBIES"), 
+                    error=conditionMessage)
+    exp <- "verbose must be a logicial value (TRUE or FALSE)"
+    msg <- paste0(base_msg, "An invalid verbose value did not generate an ", 
+                "exception with expected message." )
+    checkIdentical(obs, exp, msg)
+}
+
+## Negative padding_size value
+test.metagene_initialize_negative_padding_value <- function() {
+    obs <- tryCatch(metagene:::metagene$new(padding_size=-1), 
+                                            error=conditionMessage)
+    exp <- "padding_size must be a non-negative integer"
+    msg <- paste0(base_msg, "An negative padding_size value did not generate ",
+                "an exception with expected message." )
+    checkIdentical(obs, exp, msg)
+}
+
+## Non-integer padding_size value
+test.metagene_initialize_invalid_padding_value <- function() {
+    obs <- tryCatch(metagene:::metagene$new(padding_size="NEW_ZOMBIE"), 
+                                            error=conditionMessage)
+    exp <- "padding_size must be a non-negative integer"
+    msg <- paste0(base_msg, "An non-integer padding_size value did not ", 
+                    "generate an exception with expected message." )
+    checkIdentical(obs, exp, msg)
+}
+
