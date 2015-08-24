8c1fc0405b21426fe6f6bd3164b19830ca5803e8
Charles Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2015-08-24 15:35:57 +0000

    fix conflicts
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metagene@107720 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/demo.R
+++ R/demo.R
@@ -8,7 +8,8 @@ get_demo_bam_files <- function() {
     c(system.file("extdata/align1_rep1.bam", package="metagene"),
         system.file("extdata/align1_rep2.bam", package="metagene"),
         system.file("extdata/align2_rep1.bam", package="metagene"),
-        system.file("extdata/align2_rep2.bam", package="metagene"))
+        system.file("extdata/align2_rep2.bam", package="metagene"),
+        system.file("extdata/ctrl.bam", package="metagene"))
 }
 
 #' Get regions filenames for demo
