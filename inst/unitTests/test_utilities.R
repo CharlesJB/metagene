7f1a1e8cde43fdef919384884b6ae4a24b91372d
Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>
2015-09-16 22:24:23 -0400

    Updated the pre-computed datasets (closes #45).
    Removed the old TSS.human and TSS.mouse dataset and replaced them with
    the promoters_{hg19,hg18,mm10,mm9} datasets that are available with the
    data(promoters_xxxx) function.
    
    Removed the no longer useful functions getGenes and getGenesBiomart and
    replaced them with the get_promoter_txdb function.
    
    Signed-off-by: Charles Joly Beauparlant <charles.joly-beauparlant@crchul.ulaval.ca>

--- inst/unitTests/test_utilities.R
+++ inst/unitTests/test_utilities.R
@@ -8,29 +8,3 @@ if(FALSE) {
 }
 
 ### }}}
-
-
-###################################################
-## Test the getGenes() function
-###################################################
-
-## An invalid specie should not be accepted as an argument
-test.getGenes_not_valid_specie<- function() {
-	obs <- tryCatch(metagene:::getGenes(specie="tomato"), error=conditionMessage)
-	exp <- "Incorrect parameter for specie name.\nCurrently supported species are: \"mouse\", \"human\"."
-	checkIdentical(obs, exp, msg="getGenes() - An invalid specie argument did not generate an exception with expected message.")
-}
-
-## Human specie should return a data set with sepcific header
-test.getGenes_human<- function() {
-	data<-metagene:::getGenes("human")
-	checkTrue(class(data) == "GRanges")
-	checkTrue(length(data$feature) > 0, "getGenes() - The returned dataset has not observation.")
-}
-
-## Mouse specie should return a data set with sepcific header
-test.getGenes_mouse<- function() {
-	data<-metagene:::getGenes("mouse")
-	checkTrue(class(data) == "GRanges")
-	checkTrue(length(data$feature) > 0, "getGenes() - The returned dataset has not observation.")
-}
