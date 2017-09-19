bb36f5bc6689ab55e3419660c8730c117492d581
lippensc <lippens.cedric@gmail.com>
2017-09-19 15:15:37 -0400

    rnaseq perm test only available on chipseq

--- R/permutation.R
+++ R/permutation.R
@@ -41,6 +41,9 @@
 #' @export
 permutation_test <- function(table1, table2, sample_size, sample_count, FUN,
                             ...) {
+	if (!is.null(table1$nuc)) {
+		stop("Permutation test is only available for chipseq data.")
+	}
     stopifnot(is.data.frame(table1))
     stopifnot(is.data.frame(table2))
     stopifnot(!identical(table1, table2))
